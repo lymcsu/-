4020ae3bdc1022059520a839f1bf67f56df661af
lym <610144218@qq.com>
2016-07-29 01:53:20 +0800

    lym
    活动详情弹窗

--- js/sponsor-activity.js
+++ js/sponsor-activity.js
@@ -1,16 +1,54 @@
 $(function(){
-	$(".select-menu li.choosed").each(function(){
-		var	btnMenuChooseText = $(this).text();	
-		$(this).parent().siblings(".select-btn").find("p").text(btnMenuChooseText);
-	})
+	var	btnMenuChooseText = $('.form-select-btn.nth1').siblings(".form-select-menu").find(".choosed").text();
+	$('.form-select-btn.nth1 p').text(btnMenuChooseText);
+	var	btnMenuChooseText1 = $('.form-select-btn.nth2').siblings(".form-select-menu").find(".choosed").text();
+	$('.form-select-btn.nth2 p').text(btnMenuChooseText1);
+	var	btnMenuChooseText2 = $('.form-select-btn.nth3').siblings(".form-select-menu").find(".choosed").text();
+	$('.form-select-btn.nth3 p').text(btnMenuChooseText1);
 })
-$('.select-menu li').click(function(){
-	$(this).siblings().removeClass('choosed');
+$('.form-select-menu li').click(function(){
+	$('.form-select-menu li').removeClass('choosed');
 	$(this).addClass('choosed');
 	var	btnMenuChooseText = $(this).text();
-	$(this).parent().siblings(".select-btn").find("p").text(btnMenuChooseText);
+	$(this).parent().siblings("button").find("p").text(btnMenuChooseText);
+});
+$('body').click(function(e){
+	var _formSelect1 = $('.form-right .form-select-btn.nth1'); // 设置目标区域
+    if (!_formSelect1.is(e.target) && _formSelect1.has(e.target).length === 0) { 
+   	 	$('.form-select-btn.nth1').parent().removeClass('open');
+    } else {
+    	$('.form-select-btn.nth1').parent().toggleClass('open');
+    }
+    var _formSelect2 = $('.form-right .form-select-btn.nth2'); // 设置目标区域
+    if (!_formSelect2.is(e.target) && _formSelect2.has(e.target).length === 0) { 
+   	 	$('.form-select-btn.nth2').parent().removeClass('open');
+    } else {
+    	$('.form-select-btn.nth2').parent().toggleClass('open');
+    }
+    var _formSelect3 = $('.form-right .form-select-btn.nth3'); // 设置目标区域
+    if (!_formSelect3.is(e.target) && _formSelect3.has(e.target).length === 0) { 
+   	 	$('.form-select-btn.nth3').parent().removeClass('open');
+    } else {
+    	$('.form-select-btn.nth3').parent().toggleClass('open');
+    }
+});
+$(".form-location-input-more-btn").on("click",function(){
+	$(".form-location-input-tree").toggle();
+})
+$(".form-location-input-tree").find("a").on("click",function(){
+	var text = $(this).text();
+	$(".form-LocationInput input").val(text);
+	$(".form-LocationInput span.placeholder").hide();
+	$(".form-location-input-tree").hide();
+})
+$(function(){
+    $(".special-tree").treemenu({delay:300}).openActive();
 });
 
+$(".special-tree").on('click','a',function(){
+	$('.special-tree a').removeClass('active');
+	$(this).addClass('active');
+});
 /* 分页 */
 $('.js-tabs-div1 .pagination').twbsPagination({
 	totalPages: 35,
@@ -113,7 +151,7 @@ $('.text-div').on('click', '.choose-customer-base li', function() {
 });
 
 //新增策略
-$('.add-strategy-btn').click(function(){
+$('.top-operation-btn.nth1').click(function(){
 	$('.main-alert').fadeIn();
 })
 
@@ -146,11 +184,18 @@ $('.three-table').on('click','table tbody tr',function(){
 })
 
 
-$('.right-alert-content-main').on('click','.btn-div .operation-btn',function(){
-	$(this).removeClass('operation-btn').addClass('save-btn');
-	$('.right-alert-content-main .btn-div .cancel-btn').fadeIn();
-	$('.right-alert-content-main.nth1 .form-items input,.right-alert-content-main.nth1 .form-items textarea').attr("disabled",false);
-	$(".right-alert-content-main.nth1 .form-items .SpanSelect.disabled").removeClass('disabled').addClass('ture');
+$('.right-alert-tabs-nav').on('click','.top-operation-btn.nth2',function(){
+	if($(this).hasClass("operation-enable")){
+		$(this).removeClass("operation-enable");
+		$('.btn-div').children('.cancel-btn').fadeOut();
+		$('.right-alert-content-main.nth1 .form-items input,.right-alert-content-main.nth1 .form-items textarea').attr("disabled","disabled");
+		$(".right-alert-content-main.nth1 .form-items .SpanSelect").addClass('disabled').removeClass('ture open');
+	}else{
+		$(this).addClass("operation-enable");
+		$('.right-alert-content-main .btn-div .cancel-btn').fadeIn();
+		$('.right-alert-content-main.nth1 .form-items input,.right-alert-content-main.nth1 .form-items textarea').attr("disabled",false);
+		$(".right-alert-content-main.nth1 .form-items .SpanSelect.disabled").removeClass('disabled').addClass('ture');
+	}
 });
 
 $('.right-alert-content-main').on('click','.btn-div .cancel-btn,.btn-div .save-btn',function(){
